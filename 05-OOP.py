0bde2e762c6ed36c62b8a6ff1c5d2a3d2ff03996
Ohoud Alawad <ohoudalawad@gmail.com>
2022-04-18 02:38:22 +0300

    Add Tutorial OOP

---
+++ 05-OOP.py
@@ -0,0 +1,51 @@
+'''
+name = "Ohoud"
+
+print(type([name]))
+
+for x in [name]:
+    print(x)
+
+x = ['Ohoud',2,3,4]
+'''
+'''
+numbers = [1,2,3,4]
+
+x , *y = numbers
+
+print(x)
+
+print(y)
+'''
+
+'''
+
+    str
+    list
+    tuble
+
+'''
+
+x = 'Ohoud'
+
+y= "Ohoud"
+
+z= '''Ohoud'''  # DocString
+
+name = "\
+Ohoud  \
+Alawad "
+
+print (name)
+
+f_name = '''
+
+    Ahmed Mahmoud
+
+    age : 40
+
+        address : manareed
+
+    '''
+
+print(f_name)
