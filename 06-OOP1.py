9069fd1431ca34f8f67ad9d57db9440df68e4df1
Ohoud Alawad <ohoudalawad@gmail.com>
2022-04-21 03:45:45 +0300

    06-OOP1

---
+++ 06-OOP1.py
@@ -0,0 +1,44 @@
+
+class Calculator:
+    # method = function
+    # property = attributes
+
+    def sum(self,x,y): #method
+        return x+y
+
+    def reset():  #method
+        pass
+
+    def mul(self,x,y): #method
+        return x*y
+    def __init__(self,x,y): #constructor
+        print(f'Welcome {x} {y}')
+
+        print(self.sum(5,6))
+        
+'''
+    def __init__(self,name): #constructor
+        print(f'Welcome {name}')
+        '''
+
+    
+'''
+m = Calculator("Ahmed")
+'''
+m = Calculator(5,6)
+    
+'''
+    def __init__(self): #constructor
+        print('Welcome')
+        
+
+m = Calculator()
+'''
+'''
+#object.method_name
+print(m.sum(3,4))
+
+b= m.mul(3,4)
+print(b)
+'''
+
